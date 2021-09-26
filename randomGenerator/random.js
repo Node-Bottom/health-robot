436e61a90db064997a2ffea9ba8a627a2ecceca9
kinshuk9449 <57700700+kinshuk9449@users.noreply.github.com>
2021-09-26 14:17:39 +1000

    Update random.js

--- randomGenerator/random.js
+++ randomGenerator/random.js
@@ -1,23 +1,87 @@
+
+const mqtt = require('mqtt');
+const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser');
+const randomCoordinates = require('random-coordinates');
 const randomInt = require('random-int');
-setInterval(makePostRequest, 4000);
-const bpdata = [];
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://kinshuk:Kinshu123@cluster0.8vugi.mongodb.net/users?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function makePostRequest() {
-  const email = "jainkinshuk112@gmail.com";
-  const time = Date.now();
-  const bp = randomInt(0,10);
-  console.log(bp);
-  console.log(time);
-  const body = {
-    email,
-    time,
-    bp
-  };
-  bpdata.push(bp);
-  console.log(bpdata);
-    let res = await axios.post('http://localhost:5001/api/users', body);    
+const app = express();
 
-}
+const User = require('./models/users');
 
-makePostRequest();
+const port = 5005;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
+
+client.on('connect', () => {
+    client.subscribe('/bpData');
+    console.log('mqtt connected');
+});
+
+client.on('message', (topic, message) => {
+    if (topic == '/bpData') {
+      const data = JSON.parse(message);
+      User.findOne({"email": data.deviceId }, (err, device) => {
+        if (err) {
+          console.log(err)
+        }
+    
+        const { bpData } = device;
+        const { bp, temp } = data;
+  
+        bpData.push({ bp, temp });
+        device.bpData = bpData;
+  
+        device.save(err => {
+          if (err) {
+            console.log(err)
+          }
+        });
+      });
+    }
+  });
+
+  app.put('/sensor-data', (req, res) => {
+  
+    const { deviceId }  = req.body;
+    const bp = randomInt(100,130);
+    const temp = randomInt(95,105);
+  
+    const topic = `/bpData`;
+    const message = JSON.stringify({ deviceId, bp, temp });
+  
+    client.publish(topic, message, () => {
+      res.send('published new message');
+    });
+  });
+
+  app.post('/send-command', (req, res) => {
+    const { deviceId } = req.body;
+    const command = "test";
+    const topic = `/kin/command/${deviceId}`;
+    const body = {deviceId};
+    axios.put('http://localhost:5005/sensor-data', body);
+    client.publish(topic, command, () => {
+        res.send('published new message');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
